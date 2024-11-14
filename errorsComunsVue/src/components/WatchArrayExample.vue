77e0a231f5b6422d3bd21a2b25bcf79e85e023a4
adrianRodriguez77 <adrian.rodriguez.vegas.7e6@itb.cat>
2024-11-14 18:17:12 +0100

    Errores

---
+++ errorsComunsVue/src/components/WatchArrayExample.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { ref, watch } from 'vue'
+const numbers = ref([])
+
+watch(numbers, () => console.log('new number added'))
+watch(numbers, (nouValor, anticValor) => console.log(nouValor, anticValor));
+
+const randomNum = () => numbers.value.push(Math.random())
+</script>
+<template>
+  <button @click="randomNum">Add Random Number</button>
+</template>
